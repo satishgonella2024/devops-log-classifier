2392943077ea1637f00b4682577232c5a18cd9f7
Subrahmanya Gonella <satishgonella2024@gmail.com>
2024-12-17 17:05:55 +0000

    Refined inference script and added label mapping

--- src/infer_model.py
+++ src/infer_model.py
@@ -4,17 +4,17 @@ from transformers import pipeline
 model_id = "satishgonella/devops-log-classifier"  # Replace with your Hugging Face model repo name
 classifier = pipeline("text-classification", model=model_id)
 
-# Step 2: Log Examples for Inference
-logs = [
+label_mapping = {0: "ERROR", 1: "WARNING", 2: "INFO"}
+
+test_logs = [
     "Error: Kubernetes pod out of memory.",
     "Warning: Disk usage has reached 90%.",
     "Info: Backup process completed successfully.",
     "Error: Jenkins pipeline failed at step 3.",
-    "Warning: High memory usage detected on node01."
+    "Warning: High memory usage detected on node01.",
 ]
 
-# Step 3: Run Inference
-print("Log Classification Results:")
-for log in logs:
-    result = classifier(log)
-    print(f"Log: {log} => Prediction: {result[0]['label']}")
+for log in test_logs:
+    prediction = classifier(log)
+    label = label_mapping[int(prediction[0]['label'].split('_')[1])]
+    print(f"Log: {log} => Prediction: {label}")
